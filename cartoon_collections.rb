9d75d0a3cd225ec2cc5dd51c17cf62acaf320aec
Ni Chia <nichia@gmail.com>
2017-10-17 15:50:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,10 +23,13 @@ def find_the_cheese(array_strings) # code an argument here
     array_strings.include?(cheese_types[i])
     if true
       return cheese_types[i]
+    else
+      false
     end
     i = i + 1
   end
-  false
+
+
 end
 
 binding.pry

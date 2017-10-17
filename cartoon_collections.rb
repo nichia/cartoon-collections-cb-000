a3b8d652e97516a2aaeb62648caa716e4d173899
Ni Chia <nichia@gmail.com>
2017-10-17 15:25:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls) # code an argument here
   planeteer_calls.collect { |name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(plneteer_calls)# code an argument here
   # Your code here
 end
 

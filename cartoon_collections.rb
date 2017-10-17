7d0bb64ed4faaf0ea7c09f542fc44788d1d0229a
Ni Chia <nichia@gmail.com>
2017-10-17 14:59:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
+
+def roll_call_dwarves(dwarves_names)
   # Your code here
+  dwarves_names.each_with_index { |name, index| puts "#{index}. #{name}" }
 end
 
 def summon_captain_planet# code an argument here
